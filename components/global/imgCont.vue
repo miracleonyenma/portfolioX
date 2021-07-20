4a3b0b60f39cf9f2f7d3283f8eda32a53f1ad565
miracleonyenma <miracleficient@gmail.com>
2021-07-20 10:44:27 +0100

    feat: add img-cont compnent to load images within article

--- components/global/imgCont.vue
+++ components/global/imgCont.vue
@@ -1,27 +1,51 @@
 <template>
-  <img :src="`${srcUrl}`" :alt="alt">
+  <figure class="article-img">
+      <div class="img-cont">
+        <img :src="getImg()" :alt="alt" />
+      </div>
+    <figcaption class="caption">
+      {{ alt }}
+    </figcaption>
+  </figure>
 </template>
 
 <script>
 export default {
-    props:[
-        'src-url',
-        'alt'
-    ],
-
-    data(){
-        return{
-            // img: require(this.srcUrl)
-        }
+  props: {
+    src: {
+      type: String,
+      required: true,
     },
+    alt: {
+      type: String,
+      required: true,
+    },
+  },
 
-    mounted(){
-
-    }
-
+  methods: {
+    getImg() {
+      try {
+        return require(`~/assets/img/articles/${this.src}`)
+      } catch (err) {
+        console.warn(err)
+        return null
+      }
+    },
+  },
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@layer components{
+    .article-img{
+        @apply mt-0;
+        .img-cont{
+            @apply overflow-hidden rounded-md shadow-lg;
+        }
 
+        .caption{
+            @apply text-xs text-center mt-2;
+        }
+    }
+}
 </style>
