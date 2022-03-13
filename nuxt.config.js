fc2b0539d387cf1c9648a8681d3b897bec85b5e4
miracleonyenma <miracleficient@gmail.com>
2022-03-13 03:11:50 +0100

    update: move image generation func to netlify func

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,15 @@
-// import axios from 'axios';
-import { getHighlighter, loadTheme } from 'shiki';
-import puppeteer from 'puppeteer';
-import fs from 'fs';
+import axios from 'axios'
+// import fetch from 'node-fetch'
+import { getHighlighter, loadTheme } from 'shiki'
+// import puppeteer from 'puppeteer'
+import fs from 'fs'
 // import { promises } from 'dns';
 // import { join } from 'path';
 
+var port = 8070
+axios.defaults.baseURL = 'http://localhost' + ':' + port
+
+console.log(axios.defaults.baseURL)
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
@@ -15,12 +20,19 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Designer & Frontend Developer portfolio site. Built by Miracleio with love ❤' },
-      { hid: 'google-site-verification', name: 'google-site-verification', content: 'hRt_nx15rRTqJoV-b8mjBMKM3uH5ONJLM_qAfmXTni0' }
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Designer & Frontend Developer portfolio site. Built by Miracleio with love ❤',
+      },
+      {
+        hid: 'google-site-verification',
+        name: 'google-site-verification',
+        content: 'hRt_nx15rRTqJoV-b8mjBMKM3uH5ONJLM_qAfmXTni0',
+      },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // loading bar config
@@ -34,14 +46,17 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: ['@nuxtjs/tailwindcss', '@nuxtjs/svg', '@nuxtjs/google-analytics'],
+  buildModules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/svg',
+    '@nuxtjs/google-analytics',
+  ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
@@ -62,9 +77,10 @@ export default {
   pwa: {
     manifest: {
       name: 'Miracleio | PortfolioX',
-      description: 'Designer & Frontend Developer portfolio site. Built by Miracleio with love ❤',
+      description:
+        'Designer & Frontend Developer portfolio site. Built by Miracleio with love ❤',
       lang: 'en',
-    }
+    },
   },
 
   googleAnalytics: {
@@ -74,82 +90,98 @@ export default {
   // Add readingTime to document before rendering: https://content.nuxtjs.org/advanced#contentfilebeforeinsert
   hooks: {
     'content:file:beforeInsert': async (document) => {
-      if (document.extension === ".md") {
+      if (document.extension === '.md') {
         const stats = require('reading-time')(document.text)
 
         document.readingStats = stats
         console.log(stats)
       }
 
-      const url = process.env.COVER_GEN_URL || 'http://localhost:3000';
-
-      console.log(url);
-
-      const browser = await puppeteer.launch();
-
-      const page = await browser.newPage();
-
-      await page.setViewport({
-        width: 1200,
-        height: 628,
-        deviceScaleFactor: 1
-      })
-
       try {
-        await page.goto(url);
-
-        const formCont = await page.waitForSelector("#form-cont", {
-          hidden: true
+        console.log('ABOUT TO -->')
+        const res = await axios.post('/.netlify/functions/generate-preview', {
+          targetURL: 'https://cover-gen.netlify.app/',
+          document,
         })
 
-        const h1 = await page.waitForSelector("#content h1")
+        // axios.post()
 
-        await h1.click()
-
-        const titleInput = await page.waitForSelector("#title")
-        const descInput = await page.waitForSelector("#description")
-        const dateInput = await page.waitForSelector("#updatedAt")
-
-        await titleInput.type(`${document.title}`)
-        await descInput.type(`${document.description}`)
-        await dateInput.type(`${document.updatedAt}`)
-
-        console.log('titleInput', await titleInput.evaluate(x => {
-          return x.value
-        }));
-        console.log('descInput', await descInput.evaluate(x => {
-          return x.value
-        }));
-        console.log('dateInput', await dateInput.evaluate(x => {
-          return x.value
-        }));
-
-        await h1.click();
-
-        try {
-          await fs.promises.access(`./assets/img/articles/${document.slug}/cover.png`)
-        } catch (error) {
-          console.log(error);
-
-          try {
-            await fs.promises.mkdir(`./assets/img/articles/${document.slug}`)
-          } catch (err) {
-            console.log(err, 'unable to create path');
-          }
-        }
-
-        await page.screenshot({
-          path: `./assets/img/articles/${document.slug}/cover.png`
-        })
-
-        document.coverUrl = `${document.slug}/cover.png`
-
+        const data = await res.json()
+        console.log('===>>>', data)
       } catch (err) {
-        console.log('unable to generate image', err)
+        console.log('ERR -->', err)
       }
 
-      await browser.close()
-    }
+      // const url = process.env.COVER_GEN_URL || 'http://localhost:3000';
+
+      // console.log(url);
+
+      // const browser = await puppeteer.launch();
+
+      // const page = await browser.newPage();
+
+      // await page.setViewport({
+      //   width: 1200,
+      //   height: 628,
+      //   deviceScaleFactor: 1
+      // })
+
+      // try {
+      //   await page.goto(url);
+
+      //   const formCont = await page.waitForSelector("#form-cont", {
+      //     hidden: true
+      //   })
+
+      //   const h1 = await page.waitForSelector("#content h1")
+
+      //   await h1.click()
+
+      //   const titleInput = await page.waitForSelector("#title")
+      //   const descInput = await page.waitForSelector("#description")
+      //   const dateInput = await page.waitForSelector("#updatedAt")
+
+      //   await titleInput.type(`${document.title}`)
+      //   await descInput.type(`${document.description}`)
+      //   await dateInput.type(`${document.updatedAt}`)
+
+      //   console.log('titleInput', await titleInput.evaluate(x => {
+      //     return x.value
+      //   }));
+      //   console.log('descInput', await descInput.evaluate(x => {
+      //     return x.value
+      //   }));
+      //   console.log('dateInput', await dateInput.evaluate(x => {
+      //     return x.value
+      //   }));
+
+      //   await h1.click();
+
+      //   try {
+      //     await fs.promises.access(`./assets/img/articles/${document.slug}/cover.png`)
+      //   } catch (error) {
+      //     console.log(error);
+
+      //     try {
+      //       await fs.promises.mkdir(`./assets/img/articles/${document.slug}`)
+      //     } catch (err) {
+      //       console.log(err, 'unable to create path');
+      //     }
+      //   }
+
+      //   await page.screenshot({
+      //     path: `./assets/img/articles/${document.slug}/cover.png`
+      //   })
+
+      //   document.coverUrl = `${document.slug}/cover.png`
+
+      // } catch (err) {
+      //   console.log('unable to generate image', err)
+      // }
+
+      // await browser.close()
+      document.coverUrl = `${document.slug}/cover.png`
+    },
   },
 
   // Content module configuration: https://go.nuxtjs.dev/config-content
@@ -158,7 +190,6 @@ export default {
       async highlighter() {
         // const t = await loadTheme(join(process.cwd(), './custom-themes/ayu-dark.json'))
         const highlighter = await getHighlighter({
-
           theme: 'material-ocean',
           // theme: t
         })
@@ -166,13 +197,12 @@ export default {
         return (rawCode, lang) => {
           return highlighter.codeToHtml(rawCode, lang)
         }
-      }
-    }
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  },
+  build: {},
 
   // serverMiddleware: [{ path: '/hello', handler: '~/server-middleware/hello.js' }]
 }
