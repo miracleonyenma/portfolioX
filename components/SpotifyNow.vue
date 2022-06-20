ae7c27d3b80aded7965369ac6eb67b0f1a232b71
miracleonyenma <miracleficient@gmail.com>
2022-06-20 08:25:22 +0100

    fix: ? add activated hook to fetch 30 secs after last fetch

--- components/SpotifyNow.vue
+++ components/SpotifyNow.vue
@@ -59,7 +59,12 @@ export default {
       return song
     },
   },
-  activated() {},
+  activated() {
+    // Call fetch again if last fetch more than 30 sec ago
+    if (this.$fetchState.timestamp <= Date.now() - 30000) {
+      this.$fetch()
+    }
+  },
   async fetch() {
     const nowPlayingURL =
       process.env.SPOTIFY_NOW_PLAYING_URL ||
