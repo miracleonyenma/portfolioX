5e3975fce0a13240a9d2cb304af520a44abc699d
miracleonyenma <miracleficient@gmail.com>
2022-12-25 00:16:04 +0100

    Merry Christmas!
    - add christmas script
    - change header color

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,21 @@
+<script setup>
+const isChristmasDay = () => {
+  const today = new Date();
+  const christmasDay = new Date(today.getFullYear(), 11, 25);
+  return (
+    today.getMonth() === christmasDay.getMonth() &&
+    today.getDate() === christmasDay.getDate()
+  );
+};
+</script>
 <template>
   <main>
     <header>
       <div class="warpper p-24">
-        <h1 class="font-heading font-bold text-5xl text-han-purple">Work in progress. Hang tight!</h1>
+        <span v-if="isChristmasDay"> Merry Christmas! 🎄 </span>
+        <h1 class="font-heading font-bold text-5xl text-blue-ryb">
+          Work in progress. Hang tight!
+        </h1>
       </div>
     </header>
   </main>
