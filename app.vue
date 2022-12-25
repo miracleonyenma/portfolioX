2e0b07a15f30f79f696235f3b3e2163605e09700
miracleonyenma <miracleficient@gmail.com>
2022-12-25 07:01:59 +0100

    update: add logo and meta

--- app.vue
+++ app.vue
@@ -1,3 +1,83 @@
+<script setup lang="ts">
+import { useIsChristmas } from "./composables/special-days/useChristmas";
+
+const isChristmas = useIsChristmas();
+
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} | Miracleio` : "Miracleio | PortfolioX";
+  },
+  title:
+    (isChristmas.value && "Merry Christmas! 🎄") || "Miracleio | PortfolioX",
+  meta: [
+    {
+      name: "description",
+      content:
+        "Designer & Frontend Developer portfolio site. Built by Miracleio with love ❤, Nuxt 3, Tailwind CSS, and TypeScript.",
+      key: "description",
+    },
+    //Open Graph
+    {
+      key: "og-type",
+      property: "og:type",
+      content: "website",
+    },
+    {
+      key: "og-url",
+      property: "og:url",
+      content: `https://miracleio.me/`,
+    },
+    {
+      key: "og-title",
+      property: "og:title",
+      content: `Miracleio | PortfolioX`,
+    },
+    {
+      key: "og-description",
+      property: "og:description",
+      content: `Designer & Frontend Developer portfolio site. Built by Miracleio with love ❤`,
+    },
+    {
+      key: "og-image",
+      property: "og:image",
+      content: `https://miracleio.me/img/cover.png`,
+    },
+    //Twitter
+    {
+      key: "twitter-card",
+      property: "twitter:card",
+      content: "summary_large_image",
+    },
+    {
+      key: "twitter-url",
+      property: "twitter:url",
+      content: `https://miracleio.me/`,
+    },
+    {
+      key: "twitter-title",
+      property: "twitter:title",
+      content: `Miracleio | PortfolioX`,
+    },
+    {
+      key: "twitter-description",
+      property: "twitter:description",
+      content: `Designer & Frontend Developer portfolio site. Built by Miracleio with love ❤`,
+    },
+    {
+      key: "twitter-image",
+      property: "twitter:image",
+      content: `https://miracleio.me/img/cover.png`,
+    },
+  ],
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: isChristmas ? "/logo-christmas.png" : "/logo.png",
+    },
+  ],
+});
+</script>
 <template>
   <NuxtPage />
 </template>
---
+++ composables/special-days/useChristmas.ts
@@ -0,0 +1,5 @@
+export const useIsChristmas = () =>
+  useState<boolean>("is-christmas", () => {
+    const today = new Date();
+    return today.getMonth() === 11 && today.getDate() === 25;
+  });
